4fc9db337e348b15bbd42782a1e264d85c79264b
Mohammad Sadegh Heidari <MohammadHeidari119@gmail.com>
2022-09-19 21:03:36 +0430

    add creat

---
+++ Resturan.Presentaion/Areas/Admin/Pages/Category/Creat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Resturan.Presentation.Areas.Admin.Pages.Category
+{
+    public class CreatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
